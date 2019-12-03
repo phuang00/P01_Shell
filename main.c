b1ea2e83b0f61bd13e13dfa61462191246b2e4c0
phuang00 <phuang00@stuy.edu>
2019-12-03 15:33:30 -0500

    fixed input from stdin

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main() {
   char cmd[100];
   fgets(cmd, 100, stdin);
-  while (strcmp(cmd, "exit") != 0){
-    fgets(&cmd, 100, stdin);
+  while (strcmp(cmd, "exit\n") != 0){
+    fgets(cmd, 100, stdin);
   }
   printf("hi\n");
   return 0;
