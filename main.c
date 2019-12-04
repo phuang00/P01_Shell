8c726391c79214db6e7ab0dc3245d6885624e1e7
phuang00 <phuang00@stuy.edu>
2019-12-04 15:32:23 -0500

    shortened code

--- main.c
+++ main.c
@@ -4,17 +4,14 @@ int main() {
   char cmd[100];
   while (strcmp(cmd, "exit\n") != 0){
     fgets(cmd, 100, stdin);
+    cmd[strlen(cmd) - 1] = 0;
     char ** tokens = parse_args(cmd);
-    int ret = strncmp(tokens[0], "cd", 2);
-    if (ret == 0){
-      char name[100];
-      strcpy(name, tokens[1]);
-      name[strlen(name) - 1] = 0; //terminating null
-      chdir(name);
+    if (strcmp(tokens[0], "cd\n")){
+      chdir(tokens[1]);
       if (errno != 0){
         printf("Errno: %d %s\n", errno, strerror(errno));
       }
-      printf("%s\n", getcwd(name, 100));
+      printf("%s\n", getcwd(tokens[1], 99));
     }
   }
   return 0;
