cd320231e7080aca3ca7013af2e8fb3e89f8781c
phuang00 <phuang00@stuy.edu>
2019-12-04 15:36:49 -0500

    fixed bug in cd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main() {
     fgets(cmd, 100, stdin);
     cmd[strlen(cmd) - 1] = 0;
     char ** tokens = parse_args(cmd);
-    if (strcmp(tokens[0], "cd\n")){
+    if (strcmp(tokens[0], "cd") == 0){
       chdir(tokens[1]);
       if (errno != 0){
         printf("Errno: %d %s\n", errno, strerror(errno));
