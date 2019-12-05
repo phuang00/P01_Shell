9438741644d82821282a81d50f78ab14c9746fff
phuang00 <phuang00@stuy.edu>
2019-12-05 15:26:10 -0500

    implemented reading multiple commands in one line

--- main.c
+++ main.c
@@ -5,18 +5,25 @@ int main() {
   while (strcmp(cmd, "exit") != 0){
     fgets(cmd, 100, stdin);
     cmd[strlen(cmd) - 1] = 0;
-    char ** tokens = parse_args(cmd);
-    if (strcmp(tokens[0], "cd") == 0){
-      chdir(tokens[1]);
-      if (errno != 0){
-        printf("Errno: %d %s\n", errno, strerror(errno));
+    char * args;
+    char * input = cmd;
+    while (input){
+      args = strsep(&input, ";");
+      char ** tokens = parse_args(args);
+      if (strcmp(tokens[0], "cd") == 0){
+        chdir(tokens[1]);
+        if (errno != 0){
+          printf("Errno: %d %s\n", errno, strerror(errno));
+        }
+        printf("%s\n", getcwd(tokens[1], 100));
       }
-      printf("%s\n", getcwd(tokens[1], 100));
-    }
-    else if (strcmp(cmd, "exit") != 0){
-      int id = fork();
-      if (!id){
-        execvp(tokens[0], tokens);
+      else if (strcmp(args, "exit") != 0){
+        if (fork() == 0){
+          execvp(tokens[0], tokens);
+        }
+        else{
+          wait(NULL);
+        }
       }
     }
   }
