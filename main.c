1b7f1619b456718e1b01007a17a91f34b63907ea
phuang00 <phuang00@stuy.edu>
2019-12-11 17:20:53 -0500

    freeing memory

--- main.c
+++ main.c
@@ -7,19 +7,29 @@ int main() {
     line[strlen(line) - 1] = 0;
     char ** cmds = parse_args(line, ";");
     int i = 0;
+    int j;
     while (cmds[i] != NULL && strcmp(cmds[i], "exit") != 0){
       char ** cmd = parse_args(cmds[i], "|");
+      printf("%s \n", cmd[0]);
       if (cmd[1]){
         piping(cmd);
+	for (j = 0; cmd[j] != NULL; j++){
+	  free(cmd[j]);
+	}
       }
       else{
         char ** tokens = parse_args(cmds[i], " ");
         run_cmd(tokens);
+	for (j = 0; tokens[j] != NULL; j++){
+	  free(tokens[j]);
+	}
         free(tokens);
       }
-      free(cmds[i]);
-      i++;
       free(cmd);
+      i++;
+    }
+    for (j = 0; cmds[j] != NULL; j++){
+      free(cmds[j]);
     }
     free(cmds);
   }
