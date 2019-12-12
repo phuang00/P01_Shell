1eb0796db133cbffbe1466881729933f31df9da6
phuang00 <phuang00@stuy.edu>
2019-12-11 22:05:07 -0500

    somewhat fixed freeing?

--- main.c
+++ main.c
@@ -10,26 +10,16 @@ int main() {
     int j;
     while (cmds[i] != NULL && strcmp(cmds[i], "exit") != 0){
       char ** cmd = parse_args(cmds[i], "|");
-      printf("%s \n", cmd[0]);
       if (cmd[1]){
         piping(cmd);
-	for (j = 0; cmd[j] != NULL; j++){
-	  free(cmd[j]);
-	}
       }
       else{
         char ** tokens = parse_args(cmds[i], " ");
         run_cmd(tokens);
-	for (j = 0; tokens[j] != NULL; j++){
-	  free(tokens[j]);
-	}
         free(tokens);
       }
       free(cmd);
       i++;
-    }
-    for (j = 0; cmds[j] != NULL; j++){
-      free(cmds[j]);
     }
     free(cmds);
   }
